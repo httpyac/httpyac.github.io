f04b14990fae8af29c4359e427e72a87cb2655fa
Andreas Weber <weber.andreas@gmail.com>
2021-10-06 20:05:46 +0200

    feat: provide example for use of .httpyac.json AnWeber/vscode-httpyac#71

--- examples/.httpyac.js
+++ examples/.httpyac.js
@@ -1,19 +1,8 @@
 module.exports = {
-	"log": {
-		"responseBodyLength": 0,
-		"prettyPrint": true
-	},
-	"clientCertificates": {
-		"client.badssl.com": {
-			"pfx": "./variables/badssl.com-client.p12",
-			"passphrase": "badssl.com"
+	clientCertificates: {
+		'client.badssl.com': {
+			pfx: './variables/badssl.com-client.p12',
+			passphrase: 'badssl.com'
 		}
-	},
-	configureHooks: function (api) {
-		api.hooks.responseLogging.addHook('removeSensitiveData', function (response) {
-			if (response.request) {
-				delete response.request.headers['authorization'];
-			}
-		});
 	}
 }
---
+++ examples/configfile/environments/.httpyac.js
@@ -0,0 +1,14 @@
+module.exports = {
+	environments: {
+		$shared: {
+			host: 'https://httpbin.org',
+			system: 'none'
+		},
+		dev: {
+			system: 'dev'
+		},
+		live: {
+			system: 'live'
+		}
+	}
+};
