94bce1117679ea5afa575a8bdcb9678e444afa26
AnWeber <AnWeber@users.noreply.github.com>
2022-04-26 10:56:41 +0000

    Deploying to gh-pages from @ httpyac/httpyac.github.io@8d47dd59dd675902d7fc322a69c0f647003f16b1 🚀

---
+++ assets/badges.html.f392feb0.js
@@ -0,0 +1,1 @@
+const e={key:"v-aba03d86",path:"/guide/badges.html",title:"Badges",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1650970569e3},filePathRelative:"guide/badges.md"};export{e as data};
