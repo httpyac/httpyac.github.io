6819a08899caa72fd15fefa78d76b09009720e27
AnWeber <AnWeber@users.noreply.github.com>
2023-01-12 06:18:39 +0000

    Deploying to gh-pages from @ httpyac/httpyac.github.io@2f2e2b306ccbaf609ed6846bba95fe0e199484ff 🚀

---
+++ assets/guide_installation.md.228d4cbb.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.2bd9b2de.js";
+const __pageData = JSON.parse('{"title":"Installation","description":"","frontmatter":{},"headers":[],"relativePath":"guide/installation.md"}'), _sfc_main = { name: "guide/installation.md" }, _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 3), _hoisted_4 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_4);
+}
+const installation = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  installation as default
+};
---
+++ assets/index.md.881abe14.js
@@ -0,0 +1,10 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock } from "./app.2bd9b2de.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"httpYac","tagline":"Quickly and easily send REST, SOAP, GraphQL and gRPC requests","image":{"src":"/favicon.png","alt":"httpyac"},"actions":[{"theme":"brand","text":"Get Started","link":"/guide/"}]},"features":[{"title":"Feature Rich","details":"Create and execute any REST, SOAP, GraphQL and gRPC queries from within VS Code or CLI"},{"title":"Extensible","details":"The plugin system allows the community to build and share reusable solutions to common needs."},{"title":"Manage Authentication","details":"httpYac supports many authentication out of the box (OAuth2, AWS, Basic, Digest, SSL Client Certificates)"},{"title":"Variables","details":"Built in support for variables and environments (dotenv, intellij variable support)."},{"title":"Node JS Scripting Support","details":"enrich requests with custom scripts like custom authentication or test scripts"},{"title":"Automated Test","details":"Automate manual tests and integrate them into your CI/CD pipeline"}]},"headers":[],"relativePath":"index.md"}'), _sfc_main = { name: "index.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div");
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
