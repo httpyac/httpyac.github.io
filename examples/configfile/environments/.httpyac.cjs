7966a8473b5eaedb681115d99bd962d0a3a5b5f2
Andreas Weber <weber.andreas@gmail.com>
2024-01-25 20:20:26 +0100

    feat: update gitignore and esm error

--- examples/configfile/environments/.httpyac.cjs
+++ examples/configfile/environments/.httpyac.cjs
@@ -1,13 +1,17 @@
 module.exports = {
 	environments: {
 		$shared: {
+			dev: false,
+			live: false,
 			host: 'https://httpbin.org',
 			system: 'none'
 		},
 		dev: {
+			dev: true,
 			system: 'dev'
 		},
 		live: {
+			live: true,
 			system: 'live'
 		}
 	}
